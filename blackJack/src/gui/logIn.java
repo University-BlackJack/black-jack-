63dfc5121f1156242135322c3854604274ad858c
max <max.fax.max@gmail.com>
2014-12-30 20:02:46 +0200

    sql

--- blackJack/src/gui/logIn.java
+++ blackJack/src/gui/logIn.java
@@ -1,6 +1,9 @@
 package gui;
 
+
+
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -17,12 +20,28 @@ import java.awt.image.BufferedImage;
 
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
+
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.StatementImpl;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class logIn extends JPanel {
 	private JTextField textField_userName;
 	private JPasswordField passwordField;
+	private Connection connect = null; // connect to mysql-server
+	private PreparedStatement stmt = null;
+	private boolean flag_null=false; //null string username or password
+	private boolean flag_login=false; 
+	private String UserName_server; 
+	private String Password_server;
+	private int Score_server; 
 	public logIn() {
 		setLayout(null);
 	
@@ -58,10 +77,60 @@ public class logIn extends JPanel {
 		JButton btnEnter = new JButton("enter");
 		btnEnter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				        Score Score= new Score();
-                
-						MainScreen.card.add("Score", Score);
-						MainScreen.cardLayot.show(MainScreen.card, "Score");
+				
+				 String username = null;
+				 username=textField_userName.getText();
+				 char [] pass =  passwordField.getPassword();
+				 String password = null;
+				 password = new String(pass);
+				 
+				 if(!username.equals("")&&!password.equals("")){
+					 flag_null=true;
+				 }
+				 else
+				 {
+					 JOptionPane.showMessageDialog(null,"Enter User Name and Password.");
+				 }
+				String sql = "select UserName,Password,Score FROM User;";
+				try {
+					connect = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/BlackJack","root","123456");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if(connect==null)
+					JOptionPane.showMessageDialog(null,"Connect lost ");
+				try {
+					stmt = (PreparedStatement) connect.prepareStatement(sql);
+					ResultSet rs = stmt.executeQuery(sql);
+					if(flag_null){
+						while(rs.next()){
+					         //Retrieve by column name
+					         int score_local = rs.getInt("Score");
+					         String username_local = rs.getString("UserName");
+					         String password_local = rs.getString("Password");
+					         if(username_local.equals(username)&&password_local.equals(password)){
+					        	 flag_login=true;
+					        	 UserName_server = new String(username_local);
+					        	 Password_server = new String(password_local);
+					        	 Score_server = score_local;
+					        	 break;
+					         }
+					      }
+					}
+					if(flag_null&&!flag_login){
+						JOptionPane.showMessageDialog(null,"Your user name or password is not in a database");
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if(flag_null&&flag_login)
+				{
+					Score Score= new Score();
+					MainScreen.card.add("Score", Score);
+					MainScreen.cardLayot.show(MainScreen.card, "Score");
+				}
 				
 			}
 		});
